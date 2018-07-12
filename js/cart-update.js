da24afa488d400dd3dfaaa876556fa0b3d21f559
jaridwarren <jaridwarren@gmail.com>
2018-07-12 11:08:20 -0700

    finished cart-update functionality

---
+++ js/cart-update.js
@@ -0,0 +1,6 @@
+$(".cart-button").click(function () {
+    $(".cart-icon").css({"visibility": "visible"});
+    var currentQuantity = $(".cart-icon").text();
+    var newQuantity = parseInt(currentQuantity) + 1;
+    $(".cart-icon").html(newQuantity);
+});
